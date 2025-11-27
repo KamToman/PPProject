3a0a31f5083cb341446150c1b2c8afff68954b9e
TomPaCKam <wrx81713@student.wsb.wroclaw.pl>
2025-11-27 20:48:05 +0100

    Change host for 0.0.0.0 in order to test vis ngrok

--- app.py
+++ app.py
@@ -903,6 +903,6 @@ if __name__ == '__main__':
     import os
     debug_mode = os.environ.get('FLASK_DEBUG', 'False').lower() in ('true', '1', 'yes')
     # Use 0.0.0.0 to allow external connections; for local development only use 127.0.0.1
-    host = os.environ.get('FLASK_HOST', '127.0.0.1')
+    host = os.environ.get('FLASK_HOST', '0.0.0.0')
     port = int(os.environ.get('FLASK_PORT', '5000'))
     app.run(debug=debug_mode, host=host, port=port)
